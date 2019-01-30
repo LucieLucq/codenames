9e5794e9041001d34aa07768b0f29c20c1fa1e4b
Guenael <guenael.sacheau@soprasteria.com>
2019-01-30 12:03:14 +0100

    Design connexion

--- codenames-boot/src/main/java/fr/codenames/config/SecurityConfig.java
+++ codenames-boot/src/main/java/fr/codenames/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/**").permitAll().antMatchers("/css/**").permitAll().antMatchers("/images/**").permitAll().antMatchers("/js/**").permitAll().antMatchers("/**").hasAnyRole("ADMIN", "USER").and()
+		http.authorizeRequests().antMatchers("/inscription").permitAll().antMatchers("/api/**").permitAll().antMatchers("/css/**").permitAll().antMatchers("/images/**").permitAll().antMatchers("/js/**").permitAll().antMatchers("/**").hasAnyRole("ADMIN", "USER").and()
 				.formLogin().loginPage("/connexion").loginProcessingUrl("/perform_login")
 				.defaultSuccessUrl("/accueil", true).failureUrl("/connexion?error=true").permitAll().and().logout()
 				.logoutUrl("/deconnexion").logoutSuccessUrl("/connexion").permitAll();
